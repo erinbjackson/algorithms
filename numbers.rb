66cc563fa00f86565114158b6c9fb63286a6465c
Erin Jackson <erinebjackson@gmail.com>
2022-04-14 12:49:45 -0700

    adds number palindome and collatz and fibonacci algorithms

--- numbers.rb
+++ numbers.rb
@@ -39,3 +39,63 @@
 #   fib_arr[number -1]
 # end
 # p fibonacci(21)
+
+# def leap_year(year)
+#   if year % 100 == 0 && year % 400 == 0
+#     "This is a leap year."
+#   elsif year % 100 == 0 
+#     "This is not a leap year." 
+#   elsif year % 4 == 0
+#     "This is a leap year."
+#   else
+#     "This is not a leap year."
+#   end
+  
+# end
+# p leap_year(1900)
+
+# def mult_three_five(range)
+#   div_arr =[]
+#   count = 0
+#   range.each do | num |
+#     if num % 3 == 0 || num % 5 == 0
+#       count += num
+#     end
+#   end
+#   count
+# end
+# p mult_three_five(1..1000)
+
+# def collatz(number)
+#   count = 0
+#   while number > 1
+#     if number % 2 == 0
+#       number = number / 2
+#       count += 1
+#     elsif number %2 != 2
+#       number = number * 3 + 1
+#       count += 1
+#     end
+#   end
+#   count
+# end
+# p collatz(4)
+
+def palindrome_number(range)
+  pal_arr = []
+  range.each do |x |
+    range.each do |y |
+      if (x * y).to_s  == (x * y).to_s.reverse
+        pal_arr << x * y
+      end
+    end
+  end
+  largest = pal_arr[0]
+  pal_arr.each do |num |
+    if num > largest
+      largest = num
+    end
+  end
+  largest
+end
+p palindrome_number(100..999)
